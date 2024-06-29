145aea1091bc939f467ed5ae60d26b1b0d2bf4ae
David <zamoramartinezdavid2005@gmail.com>
2024-06-29 11:48:33 +0200

    Seguimos intentando autentificar al usuario

--- PreguntasApp/models.py
+++ PreguntasApp/models.py
@@ -12,33 +12,30 @@ class Pregunta(models.Model):
     def __str__(self):
         return self.pregunta_text
 
-class UsuarioManager(BaseUserManager):
-    def create_user(self, email, name, password=None):
-        if not email:
-            raise ValueError('El usuario debe tener un correo electrónico')
-        user = self.model(
-            email=self.normalize_email(email),
-            name=name
-        )
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-
-    def create_superuser(self, email, name, password):
-        user = self.create_user(
-            email,
-            password=password,
-            name=name
-        )
-        user.is_admin = True
-        user.save(using=self._db)
-        return user
-
-    def get_by_natural_key(self, username):
-        return self.get(nickname=username)
-
-
-
+# class UsuarioManager(BaseUserManager):
+#     def create_user(self, email, name, password=None):
+#         if not email:
+#             raise ValueError('El usuario debe tener un correo electrónico')
+#         user = self.model(
+#             email=self.normalize_email(email),
+#             name=name
+#         )
+#         user.set_password(password)
+#         user.save(using=self._db)
+#         return user
+#
+#     def create_superuser(self, email, name, password):
+#         user = self.create_user(
+#             email,
+#             password=password,
+#             name=name
+#         )
+#         user.is_admin = True
+#         user.save(using=self._db)
+#         return user
+#
+#     def get_by_natural_key(self, username):
+#         return self.get(nickname=username)
 
 class Usuario(AbstractBaseUser):
 
@@ -46,7 +43,7 @@ class Usuario(AbstractBaseUser):
     e_mail = models.CharField(max_length=100, unique=True)
     rol = models.CharField(max_length=50, default='usuario')
 
-    objects = UsuarioManager()
+    objects = BaseUserManager()
 
     USERNAME_FIELD = 'e_mail'
 
